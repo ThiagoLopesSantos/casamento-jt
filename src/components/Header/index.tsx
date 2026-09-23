6bb637c0027a5c0ca89268bc1830f2eda0137c5e
ThiagoLopesSantos <thiago8343@gmail.com>
2026-09-23 03:36:42 -0300

    Mudando nome no menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -37,7 +37,7 @@ const Header = () => {
           </li>
 
           <li>
-            <a href="#galeria">Lista de Presentes</a>
+            <a href="#galeria">Galeria</a>
           </li>
 
           <li>
